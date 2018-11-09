dfca68a94c35367d0ee5227e9ca6f4595d17aaf5
wendylw <wendylw2013@gmail.com>
2018-11-10 02:49:17 +0800

    Header: Add animate

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
-(function(){
-})();
-
 (function($) {
+  function getBodyScrollTop() {
+    var scrollTop = window.pageYOffset  //用于FF
+                  || document.documentElement.scrollTop
+                  || document.body.scrollTop
+                  || 0;
+
+    return scrollTop;
+  }
+
+  function setHeader() {
+    var newScrollTop = getBodyScrollTop();
+    var bannerHeight = $('.banner').height();
+
+    if (newScrollTop >= bannerHeight && !$('.header').hasClass('fadeInDown')) {
+      $('.header').show();
+      $('.header').addClass('fadeInDown');
+      $('.header').removeClass('fadeOutUp');
+    } else if (newScrollTop < bannerHeight && !$('.header').hasClass('fadeOutUp')) {
+      $('.header').addClass('fadeOutUp');
+      $('.header').removeClass('fadeInDown');
+    }
+  }
+
+  $( window ).scroll(function() {
+    setHeader();
+  });
+
   window.onload = function () {
     $('.loading').addClass('hidden');
+    setHeader();
   };
 }(jQuery));
